0df435831c7411d71be7121dec1b70353de58472
Jonh <jonhks@192.168.1.16>
2024-09-17 17:09:21 -0600

    Deploy

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
+import Footer from "./components/Footer";
+import { db } from "./utiils/bd";
+
+const App = () => {
+  const initCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+  const [data] = useState(db);
+  const [cart, setCart] = useState(initCart);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (item) => {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  };
+
+  const removeFromCart = (id) =>
+    setCart((prev) => prev.filter((guitar) => guitar.id !== id));
+
+  const increaseQuantity = (id) => {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  };
+
+  const decreaseQuantity = (id) => {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > 1) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  };
+
+  const clearCart = () => setCart([]);
+
+  return (
+    <>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+      />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              addToCart={addToCart}
+            />
+          ))}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
